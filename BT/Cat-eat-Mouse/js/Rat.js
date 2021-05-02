878cda21e3f516ea398df227d9dbb7b4b90930d2
DoanQuan98 <NE.qkvt@gmail.com>
2021-05-02 14:58:02 +0700

    CatEatRat

---
+++ BT/Cat-eat-Mouse/js/Rat.js
@@ -0,0 +1,11 @@
+class Rat {
+    constructor(name,weight,speed) {
+        this.name = name;
+        this.weight = weight;
+        this.speed = speed;
+        this.status = true;
+    }
+    talk() {
+        alert('Chit chit');
+    }
+}
